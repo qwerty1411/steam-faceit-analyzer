a8abe9cc04126455579550c06422ed04adbcf82c
Husein  Nurkhonov <Husein@MacBook-Pro-user.local>
2025-06-24 15:50:11 +0500

    fix: deleted email from footer

--- resources/js/Pages/FaceitTracker.vue
+++ resources/js/Pages/FaceitTracker.vue
@@ -242,9 +242,6 @@
         <footer
             class="bg-gray-100 dark:bg-gray-800 text-center text-gray-600 dark:text-gray-400 py-6 mt-auto transition-colors duration-300">
             <p>© 2025 Faceit TJ. Все права защищены.</p>
-            <p>
-                <a href="mailto:contact@huseinnurkhonov16@gmail.com" class="hover:underline">huseinnurkhonov16@gmail.com</a>
-            </p>
         </footer>
     </div>
 </template>
